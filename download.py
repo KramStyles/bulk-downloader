a7cf3eec2365afa5cfe2fb2ffec00a47a382731b
Michael Jamie <kramstyles@outlook.com>
2021-12-27 22:22:15 +0100

    made it download to folder

--- download.py
+++ download.py
@@ -13,8 +13,11 @@ finally:
         url = f"http://d6.o2tv.org/How%20I%20Met%20Your%20Mother/Season%2003/How%20I%20Met%20Your%20Mother%20-%20S03E{0 if i < 10 else ''}{i}%20(O2TvSeries.Com).mp4"
         response = requests.get(url)
         if response.status_code == 200:
-            with open(f"How I Met Your Mother - S03E0{i}.mp4", 'wb') as file:
+            with open(os.path.join(dir_path, f"How I Met Your Mother - S03E0{i}.mp4"), 'wb') as file:
                 print(f'Downloading: How I Met Your Mother - S03E0{i}.mp4', )
                 for packet in response.iter_content(chunk_size=1024):
                     file.write(packet)
+        else:
+            print('Problem Dey. No connect')
+
 
