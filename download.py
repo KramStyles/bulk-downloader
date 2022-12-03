03a6cae1458de22f5aae0282d2305e346a26f03f
kramstyles <kramstyles@outlook.com>
2022-12-03 01:44:57 +0100

    made changes to download

--- download.py
+++ download.py
@@ -1,6 +1,11 @@
 import requests, os
+
 folder = input('Enter Folder: ')
-dir_path = os.path.join(os.getcwd(), folder)
+dir_path = os.path.join(os.getcwd() + '\downloads', folder)
+
+name = 'Evil'
+season = 1
+
 try:
     os.mkdir(dir_path)
 except Exception as err:
@@ -8,16 +13,17 @@ except Exception as err:
 finally:
 
     begin = 1
-    end = 20
+    end = 12
     for i in range(begin, end + 1):
-        url = f"http://d6.o2tv.org/How%20I%20Met%20Your%20Mother/Season%2003/How%20I%20Met%20Your%20Mother%20-%20S03E{0 if i < 10 else ''}{i}%20(O2TvSeries.Com).mp4"
+
+        url = f"http://d8.o2tv.org/Sleepy%20Hollow/Season%2004/Sleepy%20Hollow%20-%20S04E{0 if i < 10 else ''}{i}%20HD%20(TvShows4Mobile.Com).mp4"
         response = requests.get(url)
         if response.status_code == 200:
-            with open(os.path.join(dir_path, f"How I Met Your Mother - S03E0{i}.mp4"), 'wb') as file:
-                print(f'Downloading: How I Met Your Mother - S03E0{i}.mp4', )
+            with open(os.path.join(dir_path, f"{name} - S0{season}E0{i}.mp4"), 'wb') as file:
+                print(f'Downloading: {name} - S0{season}E0{i}.mp4', )
                 for packet in response.iter_content(chunk_size=1024):
                     file.write(packet)
         else:
-            print('Problem Dey. No connect')
-
-
+            print(url)
+            print('Problem Dey. E no connect')
+            break
